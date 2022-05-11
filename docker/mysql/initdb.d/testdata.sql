acba19b00881935c33e0a856c2aea84c0474c3b7
t-sasaki07 <tys.jill@gmail.com>
2022-05-11 12:10:55 +0900

    git ignore reflection

--- docker/mysql/initdb.d/testdata.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users VALUES
-(1, 'Tanaka', 'male', 'tanaka@test.com', 'Tokyo', '1980-04-22', NOW(), NOW()),
-(2, 'Satou', 'female', 'satou@test.com', 'Saitama', '1974-07-11', NOW(), NOW()),
-(3, 'Hujino', 'male', 'Hujino@test.com', 'Osaka', '1990-11-01', NOW(), NOW()),
-(4, 'Chiba', 'female', 'Chibaa@test.com', 'Okinawa', '1970-12-31', NOW(), NOW()),
-(5, 'Takei', 'male', 'Takei@test.com', 'Hukuoka', '1966-10-25', NOW(), NOW());
